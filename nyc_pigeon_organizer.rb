da4c975d7992ebc2e1f1ac35bef8ae3a13911079
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-07-15 15:41:05 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,9 @@ name_array = pigeon_data[:color].values.flatten
 hash = Hash[name_array.collect { |name| [name, {:color => [], :gender => [], :lives => []}] } ]
 
 
-pigeon_data[:color].flatten.each do |val|
+hash.each do ||
+  
+  
 end 
 
 
