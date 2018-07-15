99eb408bda7287ce2bd6d0cdfae0af5d689dc599
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-07-15 15:49:59 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,12 +9,6 @@ hash = Hash[name_array.collect { |name| [name, {:color => [], :gender => [], :li
 color_array = pigeon_data[:color]
 
 
-# hash.keys.each_with_index do |name, index|
-  
-#       if pigeon_data.[:color].values.include?(name)
-          
-#       hash[name][:color] << pigeon_data[:color].keys
-#       end 
 # end 
 
 
